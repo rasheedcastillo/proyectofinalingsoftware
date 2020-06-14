ff6f2e9889bac5fd34c195dc2a721aa8e8108d39
Rasheed Castillo Batista <rabdull233@hotmail.com>
2020-06-13 19:28:02 -0500

    Prueba Jenkins

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+    /* Este es un programa simple en Java...
+       NombreArchivo: "HelloWorld.java". */
+    class HelloWorld
+    {
+        // Tu programa comienza con una llamada a main().
+        // Imprime "Hola Mundo" a la ventana de la terminal.
+        public static void main(String args[])
+        {
+            System.out.println("Hola Mundo");
+        }
+    }
